01f97c055d03effaa09fe3302946aa994ccd4d6a
saghul <saghul@gmail.com>
2012-10-16 09:31:50 +0200

    Cleaned up some imports in __init__

--- eventlet/__init__.py
+++ eventlet/__init__.py
@@ -3,10 +3,7 @@ version_info = (0, 9, 17, "dev")
 __version__ = ".".join(map(str, version_info))
 
 from eventlet import greenthread
-from eventlet import greenpool
-from eventlet import queue
 from eventlet import timeout
-from eventlet import patcher
 from eventlet import convenience
 
 sleep = greenthread.sleep
@@ -15,15 +12,6 @@ spawn_after = greenthread.spawn_after
 kill = greenthread.kill
 
 Timeout = timeout.Timeout
-with_timeout = timeout.with_timeout
-
-GreenPool = greenpool.GreenPool
-GreenPile = greenpool.GreenPile
-
-Queue = queue.Queue
-
-import_patched = patcher.import_patched
-monkey_patch = patcher.monkey_patch
 
 connect = convenience.connect
 listen = convenience.listen
