4199128d2b130077fd0f55f73ccc4db99a9c1609
saghul <saghul@gmail.com>
2012-11-10 22:07:34 +0100

    Rewrote GreenPool as Pool

---
+++ eventlet/pool.py
@@ -0,0 +1,41 @@
+
+from functools import partial
+
+import eventlet
+
+from eventlet.event import Event
+from eventlet.lock import Semaphore
+
+__all__ = ['Pool']
+
+
+class Pool(object):
+
+    def __init__(self, size=1000):
+        self._size = size
+        self._lock = Semaphore(size)
+        self._running_jobs = 0
+        self._end_event = Event()
+        self._end_event.set()
+
+    def spawn(self, func, *args, **kw):
+        self._lock.acquire()
+        self._running_jobs += 1
+        self._end_event.clear()
+        eventlet.spawn(self._runner, partial(func, *args, **kw))
+
+    def join(self, timeout=None):
+        self._end_event.wait(timeout)
+
+    def _runner(self, func):
+        try:
+            func()
+        finally:
+            self._running_jobs -= 1
+            if self._running_jobs == 0:
+                self._end_event.set()
+            self._lock.release()
+
+    def __repr__(self):
+        return '<%s(size=%d), %d running jobs>' % (self.__class__.__name__, self._size, self._running_jobs)
+
