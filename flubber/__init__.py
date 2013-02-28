ba4c44dabddedf54de84aa2d607cb90a7b7fb651
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-03-01 00:23:29 +0100

    Added Channel to flubber toplevel

--- flubber/__init__.py
+++ flubber/__init__.py
@@ -4,10 +4,12 @@
 
 __version__ = '0.0.1.dev'
 
+from flubber.channel import *
 from flubber.loop import *
 from flubber.tasks import *
 
-__all__ = [loop.__all__ +
+__all__ = [channel.__all__ +
+           loop.__all__ +
            tasks.__all__ +
            ['current', '__version__']]
 
