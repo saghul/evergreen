4f7a5f862c7d37f07a1e97d66b41450449233d24
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-03-02 01:10:56 +0100

    Added default executor to EventLoop

--- flubber/loop.py
+++ flubber/loop.py
@@ -10,6 +10,7 @@ import traceback
 from collections import deque
 from threading import local
 
+from flubber.futures import TaskPoolExecutor
 from flubber.threadpool import ThreadPool
 from flubber._socketpair import SocketPair
 from flubber._tasklet import tasklet, get_current, TaskletExit
@@ -74,6 +75,7 @@ class Timer(Handler):
 
 
 class EventLoop(object):
+    DEFAULT_EXECUTOR_WORKERS = 100
 
     def __init__(self):
         global _tls
@@ -84,6 +86,7 @@ class EventLoop(object):
         self._loop.excepthook = self._handle_error
         self.tasklet = tasklet(self._run_loop)
         self.threadpool = ThreadPool(self)
+        self._default_executor = None
 
         self._started = False
 
@@ -134,6 +137,17 @@ class EventLoop(object):
         timer.start(self._timer_cb, interval, interval)
         self._timers.add(timer)
         return handler
+
+    def run_in_executor(self, executor, callback, *args, **kw):
+        if executor is None:
+            executor = self._default_executor
+            if executor is None:
+                executor = TaskPoolExecutor(self.DEFAULT_EXECUTOR_WORKERS)
+                self._default_executor = executor
+        return executor.submit(callback, *args, **kw)
+
+    def set_default_executor(self, executor):
+        self._default_executor = executor
 
     def add_reader(self, fd, callback, *args, **kw):
         handler = Handler(callback, args, kw)
@@ -259,6 +273,7 @@ class EventLoop(object):
         self._loop.excepthook = None
         self._loop = None
         self.threadpool = None
+        self._default_executor = None
 
         self._ready_processor = None
         self._ticker = None
