e3d6e870e68e5e77b18ed44fe8cf3c1ff08aa730
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-03-10 23:13:44 +0100

    Updated setup.py classifiers

--- setup.py
+++ setup.py
@@ -24,8 +24,6 @@ setup(name             = "flubber",
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.0",
-          "Programming Language :: Python :: 3.1",
           "Programming Language :: Python :: 3.2"
           "Programming Language :: Python :: 3.3"
       ],
