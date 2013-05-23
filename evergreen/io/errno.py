330517333c73d49d579ab9251525f8cdbc4d298c
Saúl Ibarra Corretgé <saghul@gmail.com>
2013-05-24 00:22:29 +0200

    Fixed Python 3 support

--- evergreen/io/errno.py
+++ evergreen/io/errno.py
@@ -9,7 +9,7 @@ errorcode = {}
 
 def _bootstrap():
     g = globals()
-    for k, v in pyuv.errno.errorcode.iteritems():
+    for k, v in pyuv.errno.errorcode.items():
         e = v[3:]
         errorcode[k] = e
         g[e] = k
